3a9a28621a6a7d30627ff916ab5f0e76fd4d1cc5
DavidTropiansky <113219225+DavidTropiansky@users.noreply.github.com>
2024-02-23 16:55:46 -0500

    Added F Express (FX)

--- public/plugins/arrivals/custom.js
+++ public/plugins/arrivals/custom.js
@@ -2,7 +2,7 @@ sf.display.ImageDrum = function() {
   return [
     ' ','0',
     '1', '2', '3', '4', '5', '6', '6X', '7', '7X',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+    'A', 'B', 'C', 'D', 'E', 'F', 'FX', 'G', 'H', 'J'
     'L', 'M', 'N', 'Q', 'R', 'GS', 'FS', 'T',
     'V', 'W', 'Z',
   ];
